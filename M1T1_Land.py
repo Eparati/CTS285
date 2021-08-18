7b14870527d44489bd251bbd34281a577c2f7411
Eparati <31897677+Eparati@users.noreply.github.com>
2021-08-18 14:25:20 -0400

    Add files via upload

---
+++ M1T1_Land.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+CTS 285
+M1T1
+Robert Land
+8/18/2021
+"""
+
+print("Robert Land")
+print("I am learning Python.")
+print("My hobbies are mostly related to video games and their modifications.")
