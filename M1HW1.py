7dffd76094473b93793e3f1fb1675c394947810d
Eparati <31897677+Eparati@users.noreply.github.com>
2021-08-25 14:38:35 -0400

    Update M1HW1.py

--- M1HW1.py
+++ M1HW1.py
@@ -45,10 +45,35 @@ def subtractor():
         num1 = float(input("Enter a number: "))
         num2 = float(input("Enter a second number: "))
         diffNum = num1-num2
-        print("The sum of your numbers is: ",diffNum)
-        sumSelect = int(input("Enter 1 to repeat, or any other number to return to main menu.\n"))
-        if sumSelect == 1:
+        print("The difference of your numbers is: ",diffNum)
+        subSelect = int(input("Enter 1 to repeat, or any other number to return to main menu.\n"))
+        if subSelect == 1:
             print("Repeating..")
         else:
             repeaterDiff = False
+def divider():
+    repeaterDiv=True
+    while repeaterDiv == True:
+        print("Your first number will be divided by your second number. Example: 3 / 2 = 1.5 ")
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter a second number: "))
+        divNum = num1/num2
+        print("The quotient of your numbers is: ",divNum)
+        divSelect = int(input("Enter 1 to repeat, or any other number to return to main menu.\n"))
+        if divSelect == 1:
+            print("Repeating..")
+        else:
+            repeaterDiv = False
+def multiplier():
+    repeaterMult=True
+    while repeaterMult == True:
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter a second number: "))
+        multNum = num1*num2
+        print("The product of your numbers is: ",multNum)
+        multSelect = int(input("Enter 1 to repeat, or any other number to return to main menu.\n"))
+        if multSelect == 1:
+            print("Repeating..")
+        else:
+            repeaterMult = False
 main()
